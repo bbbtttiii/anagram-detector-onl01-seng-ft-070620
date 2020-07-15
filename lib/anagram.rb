733b41a926ac677a4ff9130d981772777fd1e9ce
bbbtttiii <ben.iburg@gmail.com>
2020-07-15 16:42:01 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @a_word = word
+    end
+
+    def match(word_array)
+        word_array.select do |word|                                    #only return true elements in new array
+            word.chars.sort == @a_word.chars.sort                      #tests the current word characters (sorted) against the array word characters (sorted)
+        end
+    end
+
+end
